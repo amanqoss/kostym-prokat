9c11031547704e5f69080a826b03b96360fca4a9
Ernur <amankosernur2@gmail.com>
2026-08-06 12:24:46 +0500

    fixing baggs and db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,24 @@
-import { defineConfig } from "drizzle-kit";
+// import { defineConfig } from "drizzle-kit";
+
+// export default defineConfig({
+//   out: "./drizzle",
+//   schema: "./src/db/schema.ts",
+//   dialect: "sqlite",
+//   dbCredentials: {
+//     url: process.env.DB_FILE_NAME || "sqlite.db",
+//   },
+// });
+
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env.local' });
+
+import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  out: "./drizzle",
-  schema: "./src/db/schema.ts",
-  dialect: "sqlite",
+  schema: './src/db/schema.ts', // путь к вашим схемам (таблицам)
+  out: './drizzle',
+  dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DB_FILE_NAME || "sqlite.db",
+    url: process.env.DATABASE_URL!,
   },
 });
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,16 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import Database from "better-sqlite3";
-import * as schema from "./schema";
+// import { drizzle } from "drizzle-orm/better-sqlite3";
+// import Database from "better-sqlite3";
+// import * as schema from "./schema";
 
-const sqlite = new Database(process.env.DB_FILE_NAME || "sqlite.db");
-sqlite.pragma("journal_mode = WAL");
+// const sqlite = new Database(process.env.DB_FILE_NAME || "sqlite.db");
+// sqlite.pragma("journal_mode = WAL");
 
-export const db = drizzle(sqlite, { schema });
+// export const db = drizzle(sqlite, { schema });
+
+
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import * as schema from './schema'; // путь к файлу со схемами
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
